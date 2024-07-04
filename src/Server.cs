247ee4d5238b9ab17bbd2e4701f4163b4dc69ab9
douglasjtds <douglasjtds@gmail.com>
2024-07-04 00:11:07 -0300

    extract URL path

--- src/Server.cs
+++ src/Server.cs
@@ -2,11 +2,25 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-var socket = server.AcceptSocket(); // wait for client
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+TcpListener server = new (IPAddress.Any, 4221);
+try
+{
+    server.Start();
+    var socket = server.AcceptSocket();
+    var responseBuffer = new byte[1024];
+    var received = socket.ReceiveAsync(responseBuffer);
+    var response = Encoding.UTF8.GetString(responseBuffer, 0, await received);
+    var parseResponse = response.Split("\r\n");
+    if (parseResponse[0] == "GET / HTTP/1.1")
+    {
+        socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+    } else {
+        socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
+    }
+}
+finally
+{
+    server.Stop();
+}
