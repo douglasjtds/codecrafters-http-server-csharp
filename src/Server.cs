4beb2e38809764c459bfa67b7b4f5cbd16f45e5e
Douglas Tertuliano <douglasjtds@gmail.com>
2024-07-09 13:42:46 -0300

    CLI tests (cli-test-1720543366790)

--- src/Server.cs
+++ src/Server.cs
@@ -13,10 +13,14 @@ try
     var received = socket.ReceiveAsync(responseBuffer);
     var response = Encoding.UTF8.GetString(responseBuffer, 0, await received);
     var parseResponse = response.Split("\r\n");
+    string str = "abc";
     if (parseResponse[0] == "GET / HTTP/1.1")
     {
         socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
-    } else {
+    } else if (parseResponse[0] == $"GET /echo/{str} HTTP/1.1"){
+        socket.Send(Encoding.UTF8.GetBytes($"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n{str}"));
+    }
+    else {
         socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
     }
 }
