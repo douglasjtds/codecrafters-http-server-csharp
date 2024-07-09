f59595a2dfff419d95280d96c733447183758bed
Douglas Tertuliano <douglasjtds@gmail.com>
2024-07-09 19:02:49 -0300

    read reader

--- src/Server.cs
+++ src/Server.cs
@@ -6,29 +6,36 @@ TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 while (true)
 {
-    TcpClient client = server.AcceptTcpClient();
-    NetworkStream stream = client.GetStream();
-    byte[] buffer = new byte[1024];
-    int bytesRead = stream.Read(buffer, 0, buffer.Length);
-    string request = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-    string[] lines = request.Split(new string[] { "\r\n" }, StringSplitOptions.None);
-    string[] startLineParts = lines[0].Split(' ');
-    string response;
-    if (startLineParts[1] == "/")
-    {
-        response = "HTTP/1.1 200 OK\r\n\r\n";
-    }
-    else if (startLineParts[1].StartsWith("/echo/"))
-    {
-        string message = startLineParts[1].Substring(6);
-        response =
-            $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {message.Length}\r\n\r\n{message}";
-    }
-    else
-    {
-        response = "HTTP/1.1 404 Not Found\r\n\r\n";
-    }
-    byte[] responseBytes = Encoding.ASCII.GetBytes(response);
-    stream.Write(responseBytes, 0, responseBytes.Length);
-    client.Close();
+  TcpClient client = server.AcceptTcpClient();
+  NetworkStream stream = client.GetStream();
+  byte[] buffer = new byte[1024];
+  int bytesRead = stream.Read(buffer, 0, buffer.Length);
+  string request = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+  string[] lines = request.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+  string[] startLineParts = lines[0].Split(' ');
+  string response;
+
+  if (startLineParts[1] == "/")
+  {
+    response = "HTTP/1.1 200 OK\r\n\r\n";
+  }
+  else if (startLineParts[1].StartsWith("/echo/"))
+  {
+    string message = startLineParts[1].Substring(6);
+    response =
+      $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {message.Length}" +
+        $"\r\n\r\n{message}";
+  }
+  else if (startLineParts[1].Contains("/user-agent")) { 
+    var userAgent = lines[2].Split(' ')[1];
+    response = $"HTTP/1.1 200 OK\r\n" + $"Content-Type: text/plain\r\n" +
+            $"Content-Length: {userAgent.Length}\r\n\r\n{userAgent}";
+  }
+  else
+  {
+    response = "HTTP/1.1 404 Not Found\r\n\r\n";
+  }
+  byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+  stream.Write(responseBytes, 0, responseBytes.Length);
+  client.Close();
 }
